226b20f23f1fd50d2f851b5cfaa3587ea84577d7
Teemu5 <teemu.t.salonen@aalto.fi>
2021-10-19 16:07:38 +0300

    added /dashboard /statistics

--- your-news-service/src/App.js
+++ your-news-service/src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  Switch, Route,
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const Header = () => {
+    return (
+    <h1 className="Header"><span>Your News Service</span></h1>
+    
+  )
+  }
+  const Article = ({text, title, source}) => {
+    return (
+      <div className="Article">
+    <h1>{title}</h1>
+    <h3>{text}</h3>
+    <span >source: </span><a href="https://www.hs.fi/" rel="noreferrer">{source}</a>
     </div>
-  );
+  )
+  }
+  const Dashboard = () => {
+    return (
+      <div className="App">
+        <Header/>
+        <Article className="Article" title='title' text='asfsdf dfsdf sdf. fsdfsdf sd.fdsfs .fsdfs' source='HS'/>
+      </div>
+    );
+    }
+
+  const Statistics = () => {
+    return (
+      <div className="App">
+        <Header/>
+        <div>Sports: 43</div>
+        <div>Finance: 25</div>
+        <div>Movies: 10</div>
+      </div>
+    );
+    }
+  return(
+    <div>
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/statistics">
+          <Statistics />
+        </Route>
+        <Route path="/">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
