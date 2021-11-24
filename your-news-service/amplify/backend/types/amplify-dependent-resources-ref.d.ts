6fc8e0bf2d168ddab126f588fd5d1662e699ecef
Tero <tero.koivisto@aalto.fi>
2021-11-24 14:24:47 +0200

    Fixed amplify gitignore

---
+++ your-news-service/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "yournewsservice58d95730": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
